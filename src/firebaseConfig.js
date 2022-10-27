436359460b6d0e8653febd4d5418258be28542b9
unknown <dasilvadeoliveirafabio@gmail.com>
2022-10-27 13:37:49 -0300

    teste de upload de imagens com google cloud 5

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,15 @@
 
 export const firebaseConfig ={
-    apiKey: process.env.REACT_APP_ApiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId
+    // apiKey: process.env.REACT_APP_ApiKey,
+    // authDomain: process.env.REACT_APP_authDomain,
+    // projectId: process.env.REACT_APP_projectId,
+    // storageBucket: process.env.REACT_APP_storageBucket,
+    // messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    // appId: process.env.REACT_APP_appId
+    apiKey: "AIzaSyAARHBLBtHC2ztyRZG8McCTG-J_t_XmbAY",
+    authDomain: "galeria-de-fotos-69d44.firebaseapp.com",
+    projectId: "galeria-de-fotos-69d44",
+    storageBucket: "galeria-de-fotos-69d44.appspot.com",
+    messagingSenderId: "198385976528",
+    appId: "1:198385976528:web:fcfd385bfea57f54decad5"
 }
